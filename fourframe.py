32376c2d9d6f51a19590ae6d0e5b70738603b2ab
Moto Shinriki <mshinrik@yahoo-corp.jp>
2020-11-17 19:51:44 +0900

    cartのみ切り抜き

--- fourframe.py
+++ fourframe.py
@@ -5,6 +5,7 @@ import torchvision.transforms as T
 from PIL import Image
 import numpy as np
 from collections import deque
+import copy
 
 
 class InputScreens(object):
@@ -23,15 +24,61 @@ resize_and_grayscale = T.Compose([T.ToPILImage(),
                                 T.Grayscale(num_output_channels=1),
                                 T.ToTensor()])
 
+def get_cart_location(env, screen_width):
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0) # MIDDLE OF CART
+
+def get_screen(env):
+    # Returned screen requested by gym is 400x600x3, but is sometimes larger
+    # such as 800x1200x3. Transpose it into torch order (CHW).
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+    # Cart is in the lower half, so strip off the top and bottom of the screen
+    _, screen_height, screen_width = screen.shape
+    screen = screen[:, int(screen_height*0.4):int(screen_height*0.8)]
+    view_width = int(screen_width * 0.6)
+    cart_location = get_cart_location(env, screen_width)
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2, cart_location + view_width // 2)
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # Convert to float, rescale, convert to torch tensor
+    # (this doesn't require a copy)
+    # screen = np.ascontiguousarray(screen, dtype=np.float32)
+    # Resize, and add a batch dimension (BCHW)
+    return screen
+
+def pillow_grayscale(screen):
+    gamma22LUT  = [pow(x/255.0, 2.2)*255 for x in range(256)] * 3
+    gamma045LUT = [pow(x/255.0, 1.0/2.2)*255 for x in range(256)]
+
+    screen = torch.from_numpy(np.array(screen)).permute(1, 2, 0).numpy() 
+    img = Image.fromarray(screen)
+
+    img_resize = img.resize((84, 84))
+
+    img_rgb = img_resize.convert("RGB") # any format to RGB
+    img_rgbL = img_rgb.point(gamma22LUT)
+    img_grayL = img_rgbL.convert("L")  # RGB to L(grayscale)
+    img_gray = img_grayL.point(gamma045LUT)
+    return np.asarray(img_gray)
+
 env = gym.make('CartPole-v1').unwrapped
 env.reset()
 
 screens = InputScreens()
 done = False
 while not done:
-    screen = env.render(mode='rgb_array')
-    gray_screen = resize_and_grayscale(screen).numpy()[0]
-    screens.push(gray_screen)
+    # screen = env.render(mode='rgb_array')
+    # gray_screen = resize_and_grayscale(screen).numpy()[0]
+    screen = get_screen(env)
+    # screens.push(resize_and_grayscale(screen))
+    gray_screen = copy.deepcopy(pillow_grayscale(screen))
+    screens.push(torch.from_numpy(gray_screen))
     _, _, done, _ = env.step(env.action_space.sample())
 
 env.close()
